ed7ebacde187a43b31376c4b08ab532b304c464a
Ubuntu <hawcroftj@gmail.com>
2018-03-22 04:03:16 +0000

    Installed Paperclip gem, added Paperclip image functionality to Products model. Made aesthetic changes to views.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,12 @@ class Product < ApplicationRecord
   has_many :order_details
   has_many :orders, through: :order_details
 
+  has_attached_file :image, styles: { large:  '600x600>',
+                                            medium: '300x300>',
+                                            thumb:  '150x150#' }
+  validates_attachment_content_type :image,
+                                    content_type: %r{ /\Aimage\/.*Z/ }
+
   validates :name, :description, :price, :category_id, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: true
