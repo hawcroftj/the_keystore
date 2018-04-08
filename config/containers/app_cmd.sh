c03bd47bc4caf62d482fd80ab9771228b93cfa09
Jadon H <hawcroftj@gmail.com>
2018-04-08 02:38:34 +0000

    Additional docker preparation.

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
  
-# Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
-exec bundle exec puma -v -C config/puma.rb -e $RAILS_ROOT;
+# Prefix `bundle` with `exec` so puma shuts down gracefully on SIGTERM (i.e. `docker stop`)
+RAILS_ENV=production exec bundle exec puma -v -C config/puma.rb -e $RAILS_ROOT;
