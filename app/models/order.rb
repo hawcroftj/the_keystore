79dd0b5f8f4476d3bb0490e92bce160b1e269896
Jadon H <hawcroftj@gmail.com>
2018-04-09 01:47:29 +0000

    Enabled Order creation through User cart checkout process. OrderDetails implementation still required.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
-  belongs_to :order_status
-  belongs_to :user
+  belongs_to :order_status, optional: true
+  belongs_to :user, optional: true
+
+  has_many :order_details
+  has_many :products, through: :order_details
+
+  validates :user_id, :total, :tax_total, presence: true
+  validates :total, :tax_total, numericality: true
 end
